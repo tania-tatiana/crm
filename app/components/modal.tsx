f413dbea3b697caa5faf6c02f02d196fa536a32d
Tetiana <bangchanka0310@gmail.com>
2026-06-27 11:38:49 +0300

    add criss-cross

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -39,6 +39,26 @@ export default function Modal({
             size === 'xl' ? 'sm:max-w-2xl' : 'sm:max-w-md'
           }`}
         >
+          <button
+            type="button"
+            onClick={onClose}
+            className="absolute top-4 right-5 text-gray-400 hover:text-gray-600 text-2xl font-semibold transition-colors focus:outline-none"
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M5.46967 5.46967C5.76256 5.17678 6.23744 5.17678 6.53033 5.46967L12 10.9393L17.4697 5.46967C17.7626 5.17678 18.2374 5.17678 18.5303 5.46967C18.8232 5.76256 18.8232 6.23744 18.5303 6.53033L13.0607 12L18.5303 17.4697C18.8232 17.7626 18.8232 18.2374 18.5303 18.5303C18.2374 18.8232 17.7626 18.8232 17.4697 18.5303L12 13.0607L6.53033 18.5303C6.23744 18.8232 5.76256 18.8232 5.46967 18.5303C5.17678 18.2374 5.17678 17.7626 5.46967 17.4697L10.9393 12L5.46967 6.53033C5.17678 6.23744 5.17678 5.76256 5.46967 5.46967Z"
+                fill="#0F172A"
+              />
+            </svg>
+          </button>
           {children}
         </Dialog.Panel>
       </Dialog>
